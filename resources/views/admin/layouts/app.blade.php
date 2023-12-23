dc69c3519f63b4a05d089e596d7380807f490de4
nhannt112113 <nguyentrongnhan04072000@gmail.com>
2023-12-23 22:52:11 +0700

    create product 1

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -10,6 +10,7 @@
 	<!-- Font Awesome -->
 	<link rel="stylesheet" href="{{asset('admin-assets/plugins/fontawesome-free/css/all.min.css')}}">
 	<link rel="stylesheet" href="{{asset('admin-assets/plugins/dropzone/min/dropzone.min.css')}}">
+	<link rel="stylesheet" href="{{asset('admin-assets/plugins/summernote/summernote.min.css')}}">
 	<!-- Theme style -->
 	<link rel="stylesheet" href="{{asset('admin-assets/css/adminlte.min.css')}}">
 	<link rel="stylesheet" href="{{asset('admin-assets/css/custom.css')}}">
@@ -83,6 +84,7 @@
 	<script src="{{asset('admin-assets/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 	<!-- AdminLTE App -->
 	<script src="{{asset('admin-assets/js/adminlte.min.js')}}"></script>
+	<script src="{{asset('admin-assets/plugins/summernote/summernote.min.js')}}"></script>
 	<!-- AdminLTE for demo purposes -->
 	<script src="{{asset('admin-assets/js/demo.js')}}"></script>
 	<script src="{{asset('admin-assets/plugins/dropzone/min/dropzone.min.js')}}"></script>
@@ -91,6 +93,12 @@
 			headers: {
 				'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
 			}
+		})
+
+		$(document).ready(function() {
+			$('.summernote').summernote({
+				height:250
+			})
 		})
 	</script>
 	@yield('customJS')
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\admin\AdminLoginController;
 use App\Http\Controllers\admin\BrandController;
 use App\Http\Controllers\admin\CategoryController;
 use App\Http\Controllers\admin\HomeController;
+use App\Http\Controllers\admin\ProductController;
+use App\Http\Controllers\admin\ProductSubCategoryController;
 use App\Http\Controllers\admin\SubCategoryController;
 use App\Http\Controllers\admin\TempImagesController;
 use Illuminate\Http\Request;
@@ -56,6 +58,14 @@ Route::group(['prefix' => 'admin'], function () {
         Route::get('/brands/{subCategory}/edit', [BrandController::class, 'edit'])->name('brands.edit');
         Route::put('/brands/{subCategory}', [BrandController::class, 'update'])->name('brands.update');
         Route::delete('/brands/{subCategory}', [BrandController::class, 'destroy'])->name('brands.delete');
+
+        Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+        Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+        Route::get('/product-subcategories', [ProductSubCategoryController::class, 'index'])->name('product-subcategories.index');
+        Route::post('/products', [ProductController::class, 'store'])->name('products.store');
+        Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
+        Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
+        Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.delete');
         
         Route::post('/upload-temp-image', [TempImagesController::class, 'create'])->name('temp-images.create');
 
