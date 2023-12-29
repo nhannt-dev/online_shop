d68b1f3ac4ddcd34e9d3618b3df780b80a4c3cc3
nhannt112113 <nguyentrongnhan04072000@gmail.com>
2023-12-29 20:36:29 +0700

    apply filter product

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -5,15 +5,61 @@ namespace App\Http\Controllers;
 use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\SubCategory;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    public function index()
+    public function index(Request $request, $categorySlug = null, $subCategorySlug = null)
     {
+        $categorySelected = '';
+        $subCategorySelected = '';
+        $brandArr = [];
+
         $categories = Category::orderBy('name', 'ASC')->with('sub_category')->where('status', 1)->get();
         $brands = Brand::orderBy('name', 'ASC')->where('status', 1)->get();
-        $products = Product::orderBy('id', 'DESC')->where('status', 1)->get();
-        return view('front.shop', compact('categories', 'brands', 'products'));
+        $products = Product::where('status', 1);
+
+        if (!empty($categorySlug)) {
+            $category = Category::where('slug', $categorySlug)->first();
+            $products = $products->where('category_id', $category->id);
+            $categorySelected = $category->id;
+        }
+        if (!empty($subCategorySlug)) {
+            $subCategory = SubCategory::where('slug', $subCategorySlug)->first();
+            $products = $products->where('sub_category_id', $subCategory->id);
+            $subCategorySelected = $subCategory->id;
+        }
+
+        if (!empty($request->get('brand'))) {
+            $brandArr = explode(',', $request->get('brand'));
+            $products = $products->whereIn('brand_id', $brandArr);
+        }
+
+        if ($request->get('price_max') != '' && $request->get('price_min') != '') {
+            if ($request->get('price_max')) {
+                $products = $products->whereBetween('price', [intval($request->get('price_min')), 1000000]);
+            } else {
+                $products = $products->whereBetween('price', [intval($request->get('price_min')), intval($request->get('price_max'))]);
+            }
+        }
+
+        $price_min = intval($request->get('price_min'));
+        $price_max = (intval($request->get('price_max')) == 0 ? 1000 : $request->get('price_max'));
+
+        if ($request->get('sort') != '') {
+            if ($request->get('sort') == 'latest') {
+                $products = $products->orderBy('id', 'DESC');
+            } elseif ($request->get('sort') == 'price_asc') {
+                $products = $products->orderBy('price', 'ASC');
+            } else {
+                $products = $products->orderBy('price', 'DESC');
+            }
+        }else {
+            $products = $products->orderBy('id', 'DESC');
+        }
+        $products = $products->get();
+        $sort = $request->get('sort');
+        return view('front.shop', compact('categories', 'brands', 'products', 'categorySelected', 'subCategorySelected', 'brandArr', 'price_min', 'price_max', 'sort'));
     }
 }
--- resources/views/front/shop.blade.php
+++ resources/views/front/shop.blade.php
@@ -32,14 +32,14 @@
                                     </button>
                                 </h2>
                                 @else
-                                <a href="#" class="nav-item nav-link">{{$category->name}}</a>
+                                <a href="{{route('front.shop', $category->slug)}}" class="nav-item nav-link {{$categorySelected==$category->id ? 'text-primary' : ''}}">{{$category->name}}</a>
                                 @endif
                                 @if ($category->sub_category->isNotEmpty())
-                                <div id="collapseOne-{{$key}}" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                <div id="collapseOne-{{$key}}" class="accordion-collapse collapse {{$categorySelected==$category->id ? 'show' : ''}}" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                                     <div class="accordion-body">
                                         <div class="navbar-nav">
                                             @foreach ($category->sub_category as $subCategory)
-                                            <a href="#" class="nav-item nav-link">{{$subCategory->name}}</a>
+                                            <a href="{{route('front.shop', [$category->slug, $subCategory->slug])}}" class="nav-item nav-link {{$subCategorySelected==$subCategory->id ? 'text-primary' : ''}}">{{$subCategory->name}}</a>
                                             @endforeach
                                         </div>
                                     </div>
@@ -61,8 +61,8 @@
                         @if ($brands->isNotEmpty())
                         @foreach ($brands as $brand)
                         <div class="form-check mb-2">
-                            <input class="form-check-input" type="checkbox" name="brand[]" value="{{$brand->id}}" id="brand-{{$brand->id}}">
-                            <label class="form-check-label" for="flexCheckDefault">
+                            <input {{ in_array($brand->id, $brandArr) ? "checked" : ""}} class="form-check-input brand-label" type="checkbox" name="brand[]" value="{{$brand->id}}" id="brand-{{$brand->id}}">
+                            <label class="form-check-label" for="brand-{{$brand->id}}">
                                 {{$brand->name}}
                             </label>
                         </div>
@@ -77,30 +77,7 @@
 
                 <div class="card">
                     <div class="card-body">
-                        <div class="form-check mb-2">
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                            <label class="form-check-label" for="flexCheckDefault">
-                                $0-$100
-                            </label>
-                        </div>
-                        <div class="form-check mb-2">
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                            <label class="form-check-label" for="flexCheckChecked">
-                                $100-$200
-                            </label>
-                        </div>
-                        <div class="form-check mb-2">
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                            <label class="form-check-label" for="flexCheckChecked">
-                                $200-$500
-                            </label>
-                        </div>
-                        <div class="form-check mb-2">
-                            <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked">
-                            <label class="form-check-label" for="flexCheckChecked">
-                                $500+
-                            </label>
-                        </div>
+                        <input type="text" class="js-range-slider" name="my_range" value="" />
                     </div>
                 </div>
             </div>
@@ -109,14 +86,11 @@
                     <div class="col-12 pb-1">
                         <div class="d-flex align-items-center justify-content-end mb-4">
                             <div class="ml-2">
-                                <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-light dropdown-toggle" data-bs-toggle="dropdown">Sorting</button>
-                                    <div class="dropdown-menu dropdown-menu-right">
-                                        <a class="dropdown-item" href="#">Latest</a>
-                                        <a class="dropdown-item" href="#">Price High</a>
-                                        <a class="dropdown-item" href="#">Price Low</a>
-                                    </div>
-                                </div>
+                                <select name="sort" id="sort">
+                                    <option class="dropdown-item" {{($sort == 'latest') ? 'selected' : ''}} value="latest">Latest</option>
+                                    <option class="dropdown-item" {{($sort == 'price_desc') ? 'selected' : ''}} value="price_desc">Price High</option>
+                                    <option class="dropdown-item" {{($sort == 'price_asc') ? 'selected' : ''}} value="price_asc">Price Low</option>
+                                </select>
                             </div>
                         </div>
                     </div>
@@ -176,4 +150,50 @@
         </div>
     </div>
 </section>
+@endsection
+
+@section('customJS')
+<script>
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 0,
+        max: 1000,
+        from: '{{$price_min}}',
+        step: 5,
+        to: '{{$price_max}}',
+        skin: 'round',
+        max_postfix: '+',
+        prefix: '$',
+        onFinish: function() {
+            applyFilters()
+        }
+    })
+
+    var slider = $(".js-range-slider").data('ionRangeSlider')
+
+
+    $('.brand-label').change(function() {
+        applyFilters()
+    })
+
+    $('#sort').change(function() {
+        applyFilters()
+    })
+
+    function applyFilters() {
+        var brands = []
+        $('.brand-label').each(function() {
+            if ($(this).is(':checked')) {
+                brands.push($(this).val())
+            }
+        })
+        var url = '{{url()->current()}}?'
+        url += `&price_min=${slider.result.from}&price_max=${slider.result.to}`
+        if (brands.length > 0) {
+            url += '&brand=' + brands.toString()
+        }
+        url += '&sort=' + $('#sort').val()
+        window.location.href = url
+    }
+</script>
 @endsection
